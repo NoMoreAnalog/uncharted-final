1794f57a3f6bda02be27744814c6f8c1cc97abf5
Pablo Rocha <jose.rocha3@gmail.com>
2016-08-27 01:29:34 -0500

    replace PNGs with SVGs

--- imports/ui/topbar/TopBar.jsx
+++ imports/ui/topbar/TopBar.jsx
@@ -6,10 +6,10 @@ const TopBar = (props) => (
         <div className="fake-navigation"></div>
         <div className="chart-types">
             <div className="label">Available Graphs:</div>
-            <div className="chart-type"><a href="#"><img src="bar.png"/>Bar Graph</a></div>
-            <div className="chart-type"><a href="#"><img src="line.png"/>Line Graph</a></div>
-            <div className="chart-type"><a href="#"><img src="radar.png"/>Radar</a></div>
-            <div className="chart-type"><a href="#"><img src="scatter.png"/>Scatter Plot</a></div>
+            <div className="chart-type"><a href="#"><img src="bar.svg"/>Bar Graph</a></div>
+            <div className="chart-type"><a href="#"><img src="line.svg"/>Line Graph</a></div>
+            <div className="chart-type"><a href="#"><img src="radar.svg"/>Radar</a></div>
+            <div className="chart-type"><a href="#"><img src="scatter.svg"/>Scatter Plot</a></div>
         </div>
     </div>
 )
