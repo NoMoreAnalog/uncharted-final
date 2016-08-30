99b9c2e7f820310aae30955c683bc92f49c7ba8c
Pablo Rocha <jose.rocha3@gmail.com>
2016-08-29 20:25:55 -0500

    make responsive

--- imports/ui/components/topbar/TopBar.jsx
+++ imports/ui/components/topbar/TopBar.jsx
@@ -6,10 +6,10 @@ const TopBar = (props) => (
         <div className="fake-navigation"></div>
         <div className="chart-types">
             <div className="label">Available Graphs:</div>
-            <div className="chart-type"><img src="bar.svg"/>Bar Graph</div>
-            <div className="chart-type"><img src="line.svg"/>Line Graph</div>
+            <div className="chart-type"><img src="bar.svg"/>Bar</div>
+            <div className="chart-type"><img src="line.svg"/>Line</div>
             <div className="chart-type"><img src="radar.svg"/>Radar</div>
-            <div className="chart-type"><img src="scatter.svg"/>Scatter Plot</div>
+            <div className="chart-type"><img src="scatter.svg"/>Scatter</div>
         </div>
     </div>
 )
