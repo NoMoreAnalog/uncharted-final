86372336d257b41c47993c661d896c38bb8a4f36
Pablo Rocha <jose.rocha3@gmail.com>
2016-09-04 00:25:06 -0500

    chart types visibility improvements

--- imports/ui/components/topbars/ChartSelector.jsx
+++ imports/ui/components/topbars/ChartSelector.jsx
@@ -21,24 +21,12 @@ class ChartSelector extends Component {
     }
 
     componentDidMount() {
-        var menu = document.querySelector('.chart-selector');
-        var menuPosition = menu.getBoundingClientRect();
-        var placeholder = document.createElement('div');
-        placeholder.style.width = menuPosition.width + 'px';
-        placeholder.style.height = menuPosition.height + 'px';
-        var isAdded = false;
 
-        window.addEventListener('scroll', function () {
-            if (window.pageYOffset >= menuPosition.top && !isAdded) {
-                menu.classList.add('sticky');
-                menu.parentNode.insertBefore(placeholder, menu);
-                isAdded = true;
-            } else if (window.pageYOffset < menuPosition.top && isAdded) {
-                menu.classList.remove('sticky');
-                menu.parentNode.removeChild(placeholder);
-                isAdded = false;
-            }
-        });
+        $(this.chartSelector)
+            .visibility({
+                type   : 'fixed'
+            })
+        ;
     }
 
     render() {
@@ -47,7 +35,7 @@ class ChartSelector extends Component {
 
         return (
 
-            <div className="chart-selector">
+            <div className="chart-selector" ref={(ref) => this.chartSelector = ref}>
 
                 <div className="label">Available Graphs:</div>
 
