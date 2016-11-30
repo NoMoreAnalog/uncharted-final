40146c967c67d80b0620211c54e68d1b6d86187a
Pablo Rocha <jose.rocha3@gmail.com>
2016-11-29 20:57:34 -0600

    remove mobx dev tools

--- imports/ui/layouts/MainLayout.jsx
+++ imports/ui/layouts/MainLayout.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import DevTools from 'mobx-react-devtools';
+import DevTools from 'mobx-react-devtools'; // Add <DevTools /> for debugging
 import {observer} from 'mobx-react';
 
 // MainLayout component - represents the whole app
 const MainLayout = observer(['countryStore', 'indicatorStore', 'chartStore'], (props) =>
     <div>
-        <DevTools />
         {props.children}
     </div>
 )
