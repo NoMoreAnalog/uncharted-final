6f52f44750efd5240b2fc8de0f0bf0c63d7d8112
Pablo Rocha <jose.rocha3@gmail.com>
2016-12-03 18:15:09 -0600

    save api fix

--- imports/api/records.js
+++ imports/api/records.js
@@ -44,32 +44,46 @@ Meteor.methods({
 
         for (var i = 0; i < data.length; i++) {
 
-            let values = [];
-            let record;
-
-            if (data[i]._id) record = Records.findOne({country: data[i].country, indicator: data[i].indicator});
+            const record = Records.findOne({country: data[i].country, indicator: data[i].indicator});
 
             if (record) {
 
                 for (var j = 0; j < data[i].values.length; j++) {
-                    values.push({
-                        year: data[i].values[j].year,
-                        value: data[i].values[j].value,
-                        changedAt: new Date(),
-                        changedBy: Meteor.user().username,
-                        delete: data[i].values[j].delete || false
-                    });
+
+                    const index = _.findIndex(record.values, {'year': data[i].values[j].year});
+
+                    if (index > -1) {
+                        record.values[index] = {
+                            year: data[i].values[j].year,
+                            value: data[i].values[j].value,
+                            createdAt: record.values[index].createdAt,
+                            createdBy: record.values[index].createdBy,
+                            changedAt: new Date(),
+                            changedBy: Meteor.user().username,
+                            delete: data[i].values[j].delete || false
+                        };
+                    } else {
+                        record.values.push({
+                            year: data[i].values[j].year,
+                            value: data[i].values[j].value,
+                            createdAt: new Date(),
+                            createdBy: Meteor.user().username
+                        });
+                    }
+
                 }
 
-                Records.update(data[i]._id, {
+                Records.update(record._id, {
                     $set: {
-                        country: data[i].country,
-                        indicator: data[i].indicator,
-                        values: values
+                        country: record.country,
+                        indicator: record.indicator,
+                        values: record.values
                     }
                 });
 
             } else {
+
+                const values = [];
 
                 for (var j = 0; j < data[i].values.length; j++) {
                     values.push({
